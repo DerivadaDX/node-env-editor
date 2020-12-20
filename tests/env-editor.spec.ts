18f9b4a7631a778b9cb0211a596486d732cb6bce
DerivadaDX <derivadadx@gmail.com>
2020-12-20 19:11:20 -0300

    change in test of empty key

--- tests/env-editor.spec.ts
+++ tests/env-editor.spec.ts
@@ -2,7 +2,7 @@ import { EnvEditor } from '../src/env-editor';
 
 describe('Get values', () => {
 	test('Passing an empty string as key should throw', () => {
-		expect(EnvEditor.get('')).toThrow(RangeError);
+		expect(() => EnvEditor.get('')).toThrow(RangeError);
 	});
 
 	test('Get and undefined key should return undefined', () => {
