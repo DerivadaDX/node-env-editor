5969fded9071003c5e3a2792d2b2abcfc35793b7
DerivadaDX <derivadadx@gmail.com>
2020-12-20 18:27:10 -0300

    added test

--- tests/env-editor.spec.ts
+++ tests/env-editor.spec.ts
@@ -1,6 +1,10 @@
 import { EnvEditor } from '../src/env-editor';
 
 describe('Get values', () => {
+	test('Passing an empty string as key should throw', () => {
+		expect(EnvEditor.get('')).toThrow(RangeError);
+	});
+
 	test('Get and undefined key should return undefined', () => {
 		expect(EnvEditor.get('FOO_BAR')).toBeUndefined();
 	});
