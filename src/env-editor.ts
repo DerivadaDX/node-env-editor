f89238d67a2516ff147518e288d012a9b0941592
DerivadaDX <derivadadx@gmail.com>
2020-12-20 17:43:39 -0300

    declared class EnvEditor

---
+++ src/env-editor.ts
@@ -0,0 +1,15 @@
+class EnvEditor {
+
+	static get(key: string): string {
+		throw new Error('Not implemented');
+	}
+
+	static set(key: string, value: string): void {
+		throw new Error('Not implemented');
+	}
+
+	static unset(key: string): void {
+		throw new Error('Not implemented');
+	}
+
+}
