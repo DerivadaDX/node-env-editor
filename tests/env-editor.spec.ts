7ffa2faf9746db6b0546e99ef6eacf27185d04b7
DerivadaDX <derivadadx@gmail.com>
2021-01-31 18:05:31 -0300

    added testKey in env-editor.spec. change in test env-editor[1.1]

--- tests/env-editor.spec.ts
+++ tests/env-editor.spec.ts
@@ -1,9 +1,11 @@
 import { EnvEditor } from '../src/env-editor';
 
+const testKey = 'NODE_ENV_EDITOR_TEST_KEY';
+
 describe('1. Get values:', () => {
 	test('1. Get a non-existent key should return undefined.', () => {
 		expect(EnvEditor.get('')).toBeUndefined();
-		expect(EnvEditor.get('hopefully_non-existent_key')).toBeUndefined();
+		expect(EnvEditor.get(testKey)).toBeUndefined();
 	});
 
 	test('2. Get an existent key should return its value.', () => {
