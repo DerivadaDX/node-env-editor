593225cfc174dc2a6cb63534858b8447f028e029
DerivadaDX <derivadadx@gmail.com>
2021-01-31 17:59:54 -0300

    change in test 1.1 of env-editor.spec.

--- tests/env-editor.spec.ts
+++ tests/env-editor.spec.ts
@@ -1,8 +1,9 @@
 import { EnvEditor } from '../src/env-editor';
 
 describe('1. Get values:', () => {
-	test('1. Passing an empty string as key should throw', () => {
-		expect(() => EnvEditor.get('')).toThrow(RangeError);
+	test('1. Get a non-existent key should return undefined.', () => {
+		expect(EnvEditor.get('')).toBeUndefined();
+		expect(EnvEditor.get('hopefully_non-existent_key')).toBeUndefined();
 	});
 
 	test('2. Get and undefined key should return undefined', () => {
