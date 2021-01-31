753ab3c549341cb1f12c89bad46219b54cc95fba
DerivadaDX <derivadadx@gmail.com>
2021-01-31 17:56:55 -0300

    added ordinals to tests in env-editor.spec

--- tests/env-editor.spec.ts
+++ tests/env-editor.spec.ts
@@ -1,11 +1,11 @@
 import { EnvEditor } from '../src/env-editor';
 
-describe('Get values', () => {
-	test('Passing an empty string as key should throw', () => {
+describe('1. Get values:', () => {
+	test('1. Passing an empty string as key should throw', () => {
 		expect(() => EnvEditor.get('')).toThrow(RangeError);
 	});
 
-	test('Get and undefined key should return undefined', () => {
+	test('2. Get and undefined key should return undefined', () => {
 		expect(EnvEditor.get('FOO_BAR')).toBeUndefined();
 	});
 });
