6d4393a925136bfce7e34b2c54846828b4502d0c
DerivadaDX <derivadadx@gmail.com>
2021-01-31 21:03:33 -0300

    removed line in test env-editor[1.1]

--- tests/env-editor.spec.ts
+++ tests/env-editor.spec.ts
@@ -4,7 +4,6 @@ const testKey = 'NODE_ENV_EDITOR_TEST_KEY';
 
 describe('1. Get values:', () => {
 	test('1. Get a non-existent key should return undefined.', () => {
-		expect(EnvEditor.get('')).toBeUndefined();
 		expect(EnvEditor.get(testKey)).toBeUndefined();
 	});
 
